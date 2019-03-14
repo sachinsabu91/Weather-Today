e6de55317f913a1774aa0bdc71f73c06d5653041
Sachin Sabu <sachinsabu91@gmail.com>
2019-03-14 20:43:15 +0530

    Adding a color theme for navigation bars

--- Weather Today/Controllers/WeatherListTableViewController.swift
+++ Weather Today/Controllers/WeatherListTableViewController.swift
@@ -9,5 +9,8 @@
 import Foundation
 import UIKit
 class WeatherListTableViewController: UITableViewController{
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
 }
