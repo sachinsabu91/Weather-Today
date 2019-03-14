3b068610966bb616227f3c3ec3dc0dc87540eb04
Sachin Sabu <sachinsabu91@gmail.com>
2019-03-14 20:24:08 +0530

    Adding controllers

---
+++ Weather Today/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddWeatherCityViewController.swift
+//  Weather Today
+//
+//  Created by Sachin Sabu on 14/03/19.
+//  Copyright © 2019 utilityappstudio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class AddWeatherCityViewController: UIViewController{
+    
+}
---
+++ Weather Today/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherListTableViewController.swift
+//  Weather Today
+//
+//  Created by Sachin Sabu on 14/03/19.
+//  Copyright © 2019 utilityappstudio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class WeatherListTableViewController: UITableViewController{
+    
+}
